2a616e6aa07290b025c04a3af08bcad2e88fc968
ankengg99 <109346750+ankengg99@users.noreply.github.com>
2023-02-08 03:08:50 +0530

    Add files via upload

---
+++ HitApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitApplication.class, args);
+	}
+
+}
